4284030420065027acd918308df954f02f2c89f5
JuanCantu1 <109363196+JuanCantu1@users.noreply.github.com>
2024-03-22 10:54:15 -0500

    Add files via upload

---
+++ WebCam/WebCam/WebCam/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pic1 = new System.Windows.Forms.PictureBox();
+            this.Pic2 = new System.Windows.Forms.PictureBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnCapture = new System.Windows.Forms.Button();
+            this.tbFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.BtnAutoCapture = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pic1
+            // 
+            this.Pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pic1.Location = new System.Drawing.Point(80, 15);
+            this.Pic1.Margin = new System.Windows.Forms.Padding(4);
+            this.Pic1.Name = "Pic1";
+            this.Pic1.Size = new System.Drawing.Size(265, 263);
+            this.Pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic1.TabIndex = 0;
+            this.Pic1.TabStop = false;
+            // 
+            // Pic2
+            // 
+            this.Pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pic2.Location = new System.Drawing.Point(452, 15);
+            this.Pic2.Margin = new System.Windows.Forms.Padding(4);
+            this.Pic2.Name = "Pic2";
+            this.Pic2.Size = new System.Drawing.Size(265, 263);
+            this.Pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pic2.TabIndex = 1;
+            this.Pic2.TabStop = false;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(80, 286);
+            this.BtnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(121, 41);
+            this.BtnStart.TabIndex = 2;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnCapture
+            // 
+            this.BtnCapture.Location = new System.Drawing.Point(209, 286);
+            this.BtnCapture.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnCapture.Name = "BtnCapture";
+            this.BtnCapture.Size = new System.Drawing.Size(121, 41);
+            this.BtnCapture.TabIndex = 3;
+            this.BtnCapture.Text = "Capture";
+            this.BtnCapture.UseVisualStyleBackColor = true;
+            this.BtnCapture.Click += new System.EventHandler(this.BtnCapture_Click);
+            // 
+            // tbFileName
+            // 
+            this.tbFileName.Location = new System.Drawing.Point(452, 302);
+            this.tbFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbFileName.Name = "tbFileName";
+            this.tbFileName.Size = new System.Drawing.Size(264, 22);
+            this.tbFileName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(448, 286);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "File Name";
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Location = new System.Drawing.Point(80, 335);
+            this.BtnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(121, 41);
+            this.BtnStop.TabIndex = 6;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // BtnAutoCapture
+            // 
+            this.BtnAutoCapture.Location = new System.Drawing.Point(209, 335);
+            this.BtnAutoCapture.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnAutoCapture.Name = "BtnAutoCapture";
+            this.BtnAutoCapture.Size = new System.Drawing.Size(121, 41);
+            this.BtnAutoCapture.TabIndex = 7;
+            this.BtnAutoCapture.Text = "Auto Capture";
+            this.BtnAutoCapture.UseVisualStyleBackColor = true;
+            this.BtnAutoCapture.Click += new System.EventHandler(this.BtnAutoCapture_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(222, 383);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(89, 22);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 380);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Time between captures";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 441);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.BtnAutoCapture);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFileName);
+            this.Controls.Add(this.BtnCapture);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.Pic2);
+            this.Controls.Add(this.Pic1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pic1;
+        private System.Windows.Forms.PictureBox Pic2;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnCapture;
+        private System.Windows.Forms.TextBox tbFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Button BtnAutoCapture;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WebCam/WebCam/WebCam/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace WebCam
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice videoCapture;
+        private FilterInfoCollection filterInfo;
+        private int captureCount = 1;
+        private Timer autoCaptureTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set the minimum, maximum, and default values for the NumericUpDown control
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = 10;
+            numericUpDown1.Value = 5;
+
+            // Initialize and configure the timer
+            autoCaptureTimer = new Timer();
+            int interval = (int)numericUpDown1.Value*1000;
+            if (interval > 0 ){
+                autoCaptureTimer.Interval = interval;
+            }
+            else
+            {
+                autoCaptureTimer.Interval = 1000; // 1 seconds
+            }
+            autoCaptureTimer.Tick += AutoCaptureTimer_Tick;
+
+            // Subscribe to the ValueChanged event of numericUpDown1
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+        }
+
+        void StartCamera()
+        {
+            try
+            {
+                filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                videoCapture = new VideoCaptureDevice(filterInfo[0].MonikerString);
+                videoCapture.NewFrame += new NewFrameEventHandler(Camera_On);
+                videoCapture.Start();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        void Camera_On(object sender, NewFrameEventArgs eventArgs)
+        {
+            Pic1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                videoCapture.Stop();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            StartCamera();
+        }
+
+        private void BtnCapture_Click(object sender, EventArgs e)
+        {
+            Pic2.Image = Pic1.Image;
+            string fileName = @"C:\Image\" + tbFileName.Text + ".jpg";
+            var bitmap = new Bitmap(Pic2.Width, Pic2.Height);
+            Pic2.DrawToBitmap(bitmap, Pic2.ClientRectangle);
+            System.Drawing.Imaging.ImageFormat imageFormat = null;
+            imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+            bitmap.Save(fileName, imageFormat);
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            StopCamera();
+        }
+
+        private void StopCamera()
+        {
+            if (videoCapture != null && videoCapture.IsRunning)
+            {
+                videoCapture.Stop();
+                Pic1.Image = null;
+                Pic2.Image = null;
+            }
+        }
+
+        private void BtnAutoCapture_Click(object sender, EventArgs e)
+        {
+            if (!autoCaptureTimer.Enabled)
+            {
+                autoCaptureTimer.Start();
+                BtnAutoCapture.Text = "Stop Auto Capture";
+            }
+            else
+            {
+                autoCaptureTimer.Stop();
+                BtnAutoCapture.Text = "Start Auto Capture";
+            }
+        }
+
+        private void AutoCaptureTimer_Tick(object sender, EventArgs e)
+        {
+            CaptureImage();
+            captureCount++;
+        }
+
+        private void CaptureImage()
+        {
+            Pic2.Image = Pic1.Image;
+            string fileName = $@"C:\Image\capture{captureCount}.jpg";
+            var bitmap = new Bitmap(Pic2.Width, Pic2.Height);
+            Pic2.DrawToBitmap(bitmap, Pic2.ClientRectangle);
+            System.Drawing.Imaging.ImageFormat imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+            bitmap.Save(fileName, imageFormat);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int interval = (int)numericUpDown1.Value*1000;
+
+            if (interval <= 0)
+            {
+                interval = 1000; 
+                numericUpDown1.Value = interval; 
+            }
+
+            autoCaptureTimer.Interval = interval;
+        }
+    }
+}
